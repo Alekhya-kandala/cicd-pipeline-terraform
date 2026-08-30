f012a05f8a862368245ee2e53f1c5e373d6fc711
Alekhya-kandala <alekhyagoud564@gmail.com>
2026-08-30 21:19:25 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -3,5 +3,7 @@ terraform {
     bucket = "my-terraform-state-bucket-alekhya-123"
     key    = "ec2/terraform.tfstate"
     region = "ap-south-1"
+    use_lockfile = true
+    encrypt      = true
   }
 }
